fc4d517c8503ad06a0531c778fbaedd718236098
JRavey <jsf220004@utdallas.edu>
2023-11-26 21:31:03 -0600

    Mouse click detection is working

--- src/project.py
+++ src/project.py
@@ -1,6 +1,9 @@
 import random
 import pygame
 
+def on_mouse_down(event):
+  print("Mouse down at", event.pos)
+
 def main():
     pygame.init()
     resolution = (800, 600)
@@ -8,13 +11,15 @@ def main():
     backColor = pygame.Color(0, 0, 0)
     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
     running = True
+    
     while running:
         # Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                on_mouse_down(event)
         # Logic
-
         # Render/Display
         screen.fill(backColor)
         pygame.display.flip()
